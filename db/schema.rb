5aadfbf426b50fcb834984870c0997b57a37a16e
teruteru <tutituti3080@gmail.com>
2022-04-27 17:02:59 +0900

    08_board_index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_27_060905) do
+ActiveRecord::Schema.define(version: 2022_04_27_073029) do
+
+  create_table "boards", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_boards_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false

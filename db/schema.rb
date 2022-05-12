0cf5209d45bb020061f9ad764ff74652aace0ecd
teruteru <tutituti3080@gmail.com>
2022-05-12 11:14:39 +0900

    20_profile_edit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_28_085946) do
+ActiveRecord::Schema.define(version: 2022_05_11_232642) do
 
   create_table "boards", force: :cascade do |t|
     t.string "title"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 2022_04_28_085946) do
     t.string "last_name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 

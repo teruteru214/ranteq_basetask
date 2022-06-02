2fb622e710869626c13c892e2bb09a48bbea37d4
teruteru <tutituti3080@gmail.com>
2022-06-02 17:47:53 +0900

    check_admin

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,2 @@
 module ApplicationHelper
-  def page_title(page_title = '', admin = false)
-    base_title =  if admin
-                  'RUNTEQ BOARD APP(管理画面)'
-                  else
-                    'RUNTEQ BOARD APP'
-                  end
-
-    page_title.empty? ? base_title : page_title + ' | ' + base_title
-  end
-
-  def active_if(path)
-    path == controller_path ? 'active' : ''
-  end
 end
